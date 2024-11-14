6277b6d17e336cc55b0dcbc7072862b2e4983019
hilo <7amzalaaroussi@gmail.com>
2024-11-14 14:23:55 +0100

    remove some comments

--- app/src/main/java/com/example/myapplication/SystemCapabilitiesActivity.java
+++ app/src/main/java/com/example/myapplication/SystemCapabilitiesActivity.java
@@ -10,14 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class SystemCapabilitiesActivity extends AppCompatActivity {
 
@@ -25,13 +22,9 @@ public class SystemCapabilitiesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_system_capabilities);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+
         Intent intent = getIntent();
         String popup = intent.getStringExtra("message_popup");
 
